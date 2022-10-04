925b975a429a2058ddd79213531c1cc9df74bbf5
Allan <allan.kimurgor@student.moringaschool.com>
2022-10-04 12:17:28 +0300

    Fetch crud lab

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -10,6 +10,7 @@ function QuestionForm(props) {
     correctIndex: 0,
   });
 
+  
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -17,14 +18,31 @@ function QuestionForm(props) {
     });
   }
 
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions",{
+      method: "POST",
+      headers:{
+        "Content-type": "application/json",
+      },
+      body:JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    });
   }
+
 
   return (
     <section>
-      <h1>New Question</h1>
+      <h1>Add New Question</h1>
       <form onSubmit={handleSubmit}>
         <label>
           Prompt:
